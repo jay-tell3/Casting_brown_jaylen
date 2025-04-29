d017fd3a1d574d3d816da0c432e2dc8712a2d8e7
jay-tell3 <jaybroww@gmail.com>
2025-04-29 15:00:39 -0500

    done

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("type a number whole plz.... or else >:)");
-           int wholeNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("is this the number you chose? " + wholeNumber + " True or False" );
-           bool isTrue = Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine("it is " + isTrue);
+            Console.WriteLine("type a number whole plz.... or else >:)"); // asking for a number
+           int wholeNumber = Convert.ToInt32(Console.ReadLine()); //reads answer and converts answer to int
+            Console.WriteLine("is this the number you chose? " + wholeNumber + " True or False" ); //asks if it was the right answer useing the last answer then asking true or false 
+           bool isTrue = Convert.ToBoolean(Console.ReadLine()); // coverts the answer to a boolen
+            Console.WriteLine("it is " + isTrue); // tells the user the answer
                 
            
 
